e4307c23de082c5f5143d93a12fe4d34d6a1a5fe
wen <gawer521n52@gmail.com>
2023-11-28 14:28:13 +0800

    Initial Commit

---
+++ 終極密碼/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  終極密碼
+//
+//  Created by EN W on 2023/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
